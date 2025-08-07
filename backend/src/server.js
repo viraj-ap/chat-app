6e8ba4f9bf0b01a89f0a11fb7489ed20929e915c
viraj.ap <viraj25092004@gmail.com>
2025-08-07 21:56:24 +0530

    backend basic done now socket.io left

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port 5001");
